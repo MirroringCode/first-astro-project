4bb3f8974c5614d2da22a3ea4ca9cfeb0e320b3e
mirroringcode <miguelolivar999@gmail.com>
2023-05-23 21:37:01 -0400

    añadida pagina de about

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,5 +2,6 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-  site:"https://mirroringcode.github.io/first-astro-project/",
+  site:"https://mirroringcode.github.io",
+  base: "/first-astro-project",
 });
